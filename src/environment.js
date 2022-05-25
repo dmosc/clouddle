f2251516871c855a83ce09efe1ea7689c2890902
dmosc <osdavrm@gmail.com>
2022-05-25 16:59:27 -0500

    Change environment variables.

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,6 @@
-// noinspection HttpUrlsUsage
 const backendConfig = {
-  httpUrl: `http://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}`,
-  wsUrl: `ws://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}`
+  httpUrl: process.env.REACT_APP_BACKEND_HTTP,
+  wsUrl: process.env.REACT_APP_BACKEND_WS
 }
 
 export { backendConfig }
